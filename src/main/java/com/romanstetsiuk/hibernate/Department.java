d09ab825be50cd0fd77ad0d0eb78bc8f70301e4f
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-09 20:42:54 +0200

    Create Deployment Entity

---
+++ src/main/java/com/romanstetsiuk/hibernate/Department.java
@@ -0,0 +1,71 @@
+package com.romanstetsiuk.hibernate;
+
+import javax.persistence.*;
+import java.util.HashSet;
+
+@Entity
+@Table(name = "DEPARTMENT",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {DEPT_NO})})
+public class Department {
+
+    private Integer deptId;
+    private String deptNo;
+    private String deptName;
+    private String location;
+    private Set<Employee> employees = new HashSet<Employee>(0);
+
+    public Department(Integer deptId, String deptName, String location) {
+        this.deptId = deptId;
+        this.deptName = deptName;
+        this.location = location;
+    }
+
+    public Department() {
+    }
+
+    @Id
+    @Column(name = "DEPT_ID")
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    @Column(name = "DEPT_NO", length = 20, nullable = false)
+    public String getDeptNo() {
+        return deptNo;
+    }
+
+    public void setDeptNo(String deptNo) {
+        this.deptNo = deptNo;
+    }
+
+    @Column(name = "DEPT_NAME", nullable = false)
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Column(name = "LOCATION")
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "department")
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+}
